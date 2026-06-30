0a54b208e589df8604e9db35a57599990ce3dbbb
Arfa Tariq <arfatariq119@gmail.com>
2026-06-30 16:50:33 +0500

    Initialize supplier management screens

---
+++ lib/views/supplier/add_supplier_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddSupplierScreen extends StatefulWidget {
+  const AddSupplierScreen({super.key});
+
+  @override
+  State<AddSupplierScreen> createState() => _AddSupplierScreenState();
+}
+
+class _AddSupplierScreenState extends State<AddSupplierScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/views/supplier/supplier_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:stock_pilot/views/supplier/add_supplier_screen.dart';
+
+class SupplierScreen extends StatelessWidget {
+   SupplierScreen({super.key});
+  final List<Map<String,dynamic>>suppliers = [
+    {
+      'company': 'TechParts Ltd',
+    
+      'supplier': 'Ali Hassan',
+      'phone': '0312-4567890',
+      'products': 38,
+
+    },
+    {
+      'company': 'DinoTech',
+    
+      'supplier': 'Ali Hassan',
+      'phone': '0312-4567890',
+      'products': 38,
+
+    },
+    {
+      'company': 'TechParts Ltd',
+    
+      'supplier': 'Ali Hassan',
+      'phone': '0312-4567890',
+      'products': 38,
+
+    }
+
+
+
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+  
+    return Scaffold(
+      backgroundColor:Colors.white,
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context) => const AddSupplierScreen()));
+      }),
+    );
+  }
+}
